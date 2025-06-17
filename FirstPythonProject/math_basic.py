e7393fbaaca22a457889581727b978a5cc1f0b03
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-06-17 17:00:04 +0200

    returning from functions (with value, without value, no return statement), None ==> NoneType

--- FirstPythonProject/math_basic.py
+++ FirstPythonProject/math_basic.py
@@ -13,9 +13,37 @@ def print_numbers_revers(nr):
         print (i)
 
 
+def sum_of_two(a, b):
+    return a+b
+
+def no_return_fn(msg):
+    print (msg)
+
+def with_empty_return_fn(msg):
+    print(msg)
+    return
+
+
 num = 10;
 print_squares_of_numbers( num )
 print_squares_of_even_numbers( num )
 
 num = 20;
 print_numbers_revers ( num )
+
+firstNr = 10
+secondNr = 25;
+
+total = sum_of_two(firstNr, secondNr)
+
+print (f"{firstNr} + {secondNr} = {total}")
+
+print()
+msg = "method without return statement ... returns what?"
+whats_returned = no_return_fn( msg )
+print (whats_returned, "\nits type is ", type(whats_returned))
+
+print()
+msg = "method with empty return statement ... returns what?"
+whats_returned = with_empty_return_fn( msg )
+print (whats_returned, "\nits type is ", type(whats_returned))
